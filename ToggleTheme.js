e5d458828e7bee24e098afc3536e61ca2d3524e9
j100cky <jaczheng@ucdavis.edu>
2025-02-11 21:38:15 -0500

    update

--- ToggleTheme.js
+++ ToggleTheme.js
@@ -4,14 +4,36 @@ import styled from 'styled-components/native';
 import { View, ScrollView, Image } from 'react-native';
 
 // Define light and dark themes
+const lightTheme = {
+    colors:{
+        background: '#ffffff',
+        buttonBackground: '#000000',
+        buttonText: '#ffffff',
+        text: '#000000',
+        petTitleBackground: '#c3c3c3',
+    },
+};
 
+const darkTheme = {
+    colors: {
+        background: '#000000',
+        buttonBackground: '#ffffff',
+        buttonText: '#000000',
+        text: '#ffffff',
+        petTitleBackground: '#5e5e5e',
+    },
+};
 
 const ToggleTheme = () => {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
 
-
+    const changeTheme = () => {
+        setIsDarkTheme((prevTheme) => !prevTheme);
+    };
 
   return (
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+
       <Container>
         <ScrollView contentContainerStyle={{ paddingBottom: 20 }}> 
           <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', marginTop: 20 }}> {/* Add marginTop to avoid first heading being cut off */}
@@ -19,16 +41,106 @@ const ToggleTheme = () => {
           </View>
 
          {/* Define Pet Care Section */}
+        <View testID='catCareTips'>
+            <Image 
+                source={{ uri: 'https://images.unsplash.com/photo-1482434368596-fbd06cae4f89?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }}
+                style={{ width: '100%', height: 400 }}
+            />
+            <ContentSection>
+                <SectionTitle>Cat Care Tips using testID</SectionTitle>
+                <ContentText>
+                    - Litter Box Hygiene: Keep the litter box clean...{'\n'}
+                    - Scratching Posts: Provide scratching posts to prevent...{'\n'}
+                    - Grooming: Regularly brush your cat to reduce shedding and ...{'\n'}
+                    - Safe Spaces: Give cats cozy, quiet spots to relax...{'\n'}
+                    - Playtime: Use interactive toys to keep them mentally stimulated {'\n'}
+                </ContentText>
+            </ContentSection>
+        </View>
 
+        <View testID='dogCareTips'>
+            <Image 
+                source={{ uri: 'https://images.unsplash.com/photo-1482434368596-fbd06cae4f89?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }}
+                style={{ width: '100%', height: 400 }}
+            />
+            <ContentSection>
+                <SectionTitle>Dog Care Tips using testID</SectionTitle>
+                <ContentText>
+                    - Litter Box Hygiene: Keep the litter box clean...{'\n'}
+                    - Scratching Posts: Provide scratching posts to prevent...{'\n'}
+                    - Grooming: Regularly brush your cat to reduce shedding and ...{'\n'}
+                    - Safe Spaces: Give cats cozy, quiet spots to relax...{'\n'}
+                    - Playtime: Use interactive toys to keep them mentally stimulated {'\n'}
+                </ContentText>
+            </ContentSection>
+        </View>
+
+        <View testID='rabbitCareTips'>
+            <Image 
+                source={{ uri: 'https://images.unsplash.com/photo-1482434368596-fbd06cae4f89?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }}
+                style={{ width: '100%', height: 400 }}
+            />
+            <ContentSection>
+                <SectionTitle>Rabbit Care Tips using testID</SectionTitle>
+                <ContentText>
+                    - Litter Box Hygiene: Keep the litter box clean...{'\n'}
+                    - Scratching Posts: Provide scratching posts to prevent...{'\n'}
+                    - Grooming: Regularly brush your cat to reduce shedding and ...{'\n'}
+                    - Safe Spaces: Give cats cozy, quiet spots to relax...{'\n'}
+                    - Playtime: Use interactive toys to keep them mentally stimulated {'\n'}
+                </ContentText>
+            </ContentSection>
+        </View>
+
+        <View testID='fishCareTips'>
+            <Image 
+                source={{ uri: 'https://images.unsplash.com/photo-1482434368596-fbd06cae4f89?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }}
+                style={{ width: '100%', height: 400 }}
+            />
+            <ContentSection>
+                <SectionTitle>Fish Care Tips using testID</SectionTitle>
+                <ContentText>
+                    - Litter Box Hygiene: Keep the litter box clean...{'\n'}
+                    - Scratching Posts: Provide scratching posts to prevent...{'\n'}
+                    - Grooming: Regularly brush your cat to reduce shedding and ...{'\n'}
+                    - Safe Spaces: Give cats cozy, quiet spots to relax...{'\n'}
+                    - Playtime: Use interactive toys to keep them mentally stimulated {'\n'}
+                </ContentText>
+            </ContentSection>
+        </View>
+
+        <View testID='birdCareTips'>
+            <Image 
+                source={{ uri: 'https://images.unsplash.com/photo-1482434368596-fbd06cae4f89?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }}
+                style={{ width: '100%', height: 400 }}
+            />
+            <ContentSection>
+                <SectionTitle>Bird Care Tips using testID</SectionTitle>
+                <ContentText>
+                    - Litter Box Hygiene: Keep the litter box clean...{'\n'}
+                    - Scratching Posts: Provide scratching posts to prevent...{'\n'}
+                    - Grooming: Regularly brush your cat to reduce shedding and ...{'\n'}
+                    - Safe Spaces: Give cats cozy, quiet spots to relax...{'\n'}
+                    - Playtime: Use interactive toys to keep them mentally stimulated {'\n'}
+                </ContentText>
+            </ContentSection>
+        </View>
         </ScrollView>
+
+        <StyledButton onPress={changeTheme}>
+            <ButtonText>{isDarkTheme ? 'Light Mode' : 'DarkMode'}</ButtonText>
+        </StyledButton>
         
       </Container>
+              
+    </ThemeProvider>
   );
 };
 
 // Styled components
 const Container = styled.View`
   flex: 1;
+  background-color: ${(props) => props.theme.colors.background};
 `;
 
 
@@ -37,6 +149,40 @@ const StyledText = styled.Text`
   margin-bottom: 20px;
   font-style:italic;
   font-weight: bold;
+  color: ${(props) => props.theme.colors.text};
+  background: ${(props) => props.theme.colors.petTitleBackground};
+  border-radius: 10px;
+  padding: 10px 15px;
+`;
+
+const ContentSection = styled.View`
+    margin-bottom: 20px;
+    padding: 10px;
+`;
+
+const SectionTitle = styled.Text`
+    font-size: 20px;
+    color: ${(props) => props.theme.colors.text};
+    font-weight: bold;
+`;
+
+const ContentText = styled.Text`
+    font-size: 16px;
+    color: ${(props) => props.theme.colors.text};
+`;
+
+const StyledButton = styled.TouchableOpacity`
+    background-color: ${(props) => props.theme.colors.buttonBackground};
+    padding: 10px 20px;
+    border-radius: 5px;
+    aligh-items: center;
+    margin-top: 10px;
+`;
+
+const ButtonText = styled.Text`
+    color: ${(props) => props.theme.colors.buttonText};
+    font-size: 23px;
+    font-weight: bold;
 `;
 
 export default ToggleTheme;
